2d961129b9d355e1f3c77809710f9d3df1c364a2
dzoni13 <bosnja@hotmail.com>
2021-04-19 16:31:34 +0200

    changes

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,68 +1,67 @@
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import {
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_FAIL,
-  LOGIN_SUCCESS,
-  LOGOUT,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    LOGOUT,
 } from "./types";
 
 export const loadUser = () => async (dispatch) => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token);
-  }
-  try {
-    const res = await axios.get(
-      "https://coffee-restaurant-backend.herokuapp.com/api/login"
-    );
-
-    dispatch({
-      type: USER_LOADED,
-      payload: res.data,
-    });
-  } catch (err) {
-    dispatch({
-      type: AUTH_ERROR,
-    });
-  }
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+    try {
+        const res = await axios.get(
+            "https://restaurant-aplication-backend.herokuapp.com/api/login"
+        );
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR,
+        });
+    }
 };
 
 export const login = (email, password) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
 
-  const body = JSON.stringify({ email, password });
+    const body = JSON.stringify({ email, password });
 
-  try {
-    const res = await axios.post(
-      "https://coffee-restaurant-backend.herokuapp.com/api/login",
-      body,
-      config
-    );
+    try {
+        const res = await axios.post(
+            "https://restaurant-aplication-backend.herokuapp.com/api/login",
+            body,
+            config
+        );
 
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: res.data,
-    });
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data,
+        });
 
-    dispatch(loadUser());
-  } catch (err) {
-    const errors = err.response.data.errors;
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
 
-    if (errors) {
-      console.log(errors);
+        if (errors) {
+            console.log(errors);
+        }
+        dispatch({
+            type: LOGIN_FAIL,
+        });
     }
-    dispatch({
-      type: LOGIN_FAIL,
-    });
-  }
 };
 
 //LOGOUT / CLEAR PROFILE
 export const logout = () => (dispatch) => {
-  dispatch({ type: LOGOUT });
+    dispatch({ type: LOGOUT });
 };
